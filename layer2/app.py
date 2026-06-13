d3d2447fb60c691b1ad5dc010b0e7a5cc9967d41
Anand <anandology@gmail.com>
2026-06-13 11:49:54 +0530

    fix layer2 issue with reloading

--- layer2/app.py
+++ layer2/app.py
@@ -21,7 +21,8 @@ def search(query, filters):
     # apply the filters selected by the user
     return apply_filters(products, filters)
 
+# Setup the app to use our search function
+app.search = search
+
 if __name__ == "__main__":
-    # Setup the app to use our search function
-    app.search = search
     app.run()
