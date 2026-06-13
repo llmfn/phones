234fbc208093def0c2b5410bbc89d3c3419f7dfa
Anand <anandology@gmail.com>
2026-06-13 09:52:34 +0530

    [layers] Update task comment in layer1

--- layer1/app.py
+++ layer1/app.py
@@ -9,7 +9,7 @@ Pick from three search implmentations.
 
 from phonekit import Application, RecommendResponse, apply_filters, search_bm25
 
-# Task: Replace bm25 search with semantic search (see )
+# Task: Replace bm25 search with semantic search (see phonekit/search/__init__.py)
 
 def search(q, filters) -> RecommendResponse:
     products = search_bm25(q)
